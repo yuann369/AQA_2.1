8c2621be69fd075669ee8f99c78b98e26059d20d
Anna Yurasova <anna.yurasova@ssi-ru.local>
2022-09-14 11:17:38 +0400

    change

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -24,7 +25,11 @@ public class SeleniumTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
